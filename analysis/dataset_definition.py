495fb5f9562f442ada9d085d4a707202048c0d89
ts482 <65360237+ts482@users.noreply.github.com>
2024-03-25 00:20:59 +0000

    new dataset definition in progress

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,15 +1,41 @@
-from ehrql import create_dataset
-from ehrql.tables.tpp import patients, practice_registrations
+from ehrql import (
+    create_dataset,
+    codelist_from_csv,
+)
+from ehrql.tables.tpp import (
+    patients,
+    clinical_events,
+    emergency_care_attendances,
+)
+from datetime import date, timedelta
 
-dataset = create_dataset()
+first_lockdown_date = date(2023, 3, 23)
+year_before_lockdown = first_lockdown_date - timedelta(weeks=52)
 
-index_date = "2020-03-31"
+pre_covid_dataset = create_dataset()
 
-has_registration = practice_registrations.for_patient_on(
-    index_date
-).exists_for_patient()
+#this isn't working but not needed for now
+#emergency_admission_codelist = codelist_from_csv('codelists/emergency_admission_codelist.csv',
+#                                                 column = 'code')
 
-dataset.define_population(has_registration)
 
-dataset.sex = patients.sex
-dataset.age = patients.age_on(index_date)
+
+#variables
+pre_covid_dataset.sex = patients.sex
+pre_covid_dataset.age = patients.age_on(year_before_lockdown)
+
+#this line should look at discharge destination
+pre_covid_dataset.discharged_home = (emergency_care_attendances
+.sort_by(emergency_care_attendances.arrival_date)
+.last_for_patient()
+.discharge_destination == 19 
+    #19 is discharge to usual home or no abode
+)
+
+
+#with regards to last condition, 
+#not sure if this is how I specify that the patient had an admission
+pre_covid_dataset.define_population((patients.age_on(year_before_lockdown)>18)
+                                    & (patients.exists_for_patient())
+                                    & emergency_care_attendances.
+                                    arrival_date.exists_for_patient())
